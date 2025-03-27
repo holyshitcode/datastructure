52aef6a434d5e7fbb09fc360c6f5b982582b18b1
junyeong <557987@naver.com>
2025-03-27 21:47:40 +0900

    edited

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ typedef struct node {
     struct node *next;
 }node;
 
+/**
+ * 노드를 만들어서 반환함
+ */
 node *initNode(int key) {
     node *temp = (node *)malloc(sizeof(node));
     temp->key = key;
@@ -13,6 +16,10 @@ node *initNode(int key) {
     return temp;
 }
 
+/**
+ * 새로운 노드가 들어가야할 노드의 직전의 노드를 반환해줌
+ * while의 반복문조건을 동시에만족할시에 반환함
+ */
 node *findPosition(node **head, int key) {
     node *temp = *head;
     node *prev = NULL;
@@ -23,6 +30,9 @@ node *findPosition(node **head, int key) {
     return prev;
 }
 
+/**
+ * 원하는 키값의 노드를 찾고 반환시켜줌
+ */
 node *findNode(node **head, int key) {
     node *temp = *head;
     while (temp != NULL) {
@@ -34,6 +44,14 @@ node *findNode(node **head, int key) {
     return NULL;
 }
 
+/**
+ * 노드를 연결리스트에 넣음
+ * 리스트가 비었거나 헤드값이 키값보다작다면 현재의 키값을 다음노드값으로 넣어주고
+ * 현재값을 헤드에넣어줌
+ * 그외에는 findPosition()이용하여 위치를 찾고
+ * 직전 노드의 next를 현재의 next에 이어준후
+ * 직전 노드의 next를 현재노드에 설정해줌
+ */
 void insertNode(node **head, int key) {
     if(*head == NULL || (*head)->key >= key) {
         node *temp = initNode(key);
@@ -47,6 +65,10 @@ void insertNode(node **head, int key) {
     }
 }
 
+/**
+ * 지워야할 키값의 직전노드를 findPosition으로 반환받고
+ * 타겟노드의 next값을 직전노드 next값에 넣어준후 메모리해제함
+ */
 void deleteNode(node **head, int key) {
     node *target =findNode(head, key);
     if(target == NULL){
@@ -62,6 +84,30 @@ void deleteNode(node **head, int key) {
     }
 }
 
+/**
+ *  next에 현재노드의 next 세팅 1->2
+ *  현재노드의 next를 이전에있던 노드로 바꿈 최초엔 NULL
+ *  previous를 현재로 세팅해줌
+ *  현재노드를 다음노드로 이동시켜서 반복
+ */
+void reverse(node **head) {
+    node *prev = NULL;
+    node *current = *head;
+    node *next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+}
+
+/**
+ * head 부터 순차조회하여 출력함
+ */
 void pouring(node **head) {
     node *temp = *head;
     while (temp != NULL) {
@@ -71,6 +117,9 @@ void pouring(node **head) {
     printf("\n");
 }
 
+/**
+ * 노드값을 넣고 삭제하여 검증
+ */
 void unitTest() {
     //given
     node *head = NULL;
@@ -84,6 +133,9 @@ void unitTest() {
     assertEqualInt(head->key,20,null);
 }
 
+/**
+ * findNode 함수로 잘찾아지는지 검증
+ */
 void unitTest2() {
     //given
     node *head = NULL;
@@ -107,11 +159,7 @@ int main(void) {
     insertNode(&head, 4);
     insertNode(&head, 5);
     insertNode(&head, 6);
-    pouring(&head);
-    deleteNode(&head, 1);
-    deleteNode(&head, 2);
-    deleteNode(&head, 3);
-    deleteNode(&head, 4);
+    reverse(&head);
     pouring(&head);
 
     return 0;
